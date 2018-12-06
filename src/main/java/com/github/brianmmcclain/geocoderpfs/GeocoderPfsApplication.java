260de6498b3e447c6ad38e1421a622f3733dc3e3
Brian McClain <brianmmcclain@gmail.com>
2018-12-06 09:26:55 -0500

    Initial commit

---
+++ src/main/java/com/github/brianmmcclain/geocoderpfs/GeocoderPfsApplication.java
@@ -0,0 +1,12 @@
+package com.github.brianmmcclain.geocoderpfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocoderPfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocoderPfsApplication.class, args);
+	}
+}
